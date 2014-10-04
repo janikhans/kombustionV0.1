580f86a1e51ff948e87cd603074ff9b1e6fef61b
Janik Knittle <janik.knittle@gmail.com>
2014-10-04 18:54:36 +0000

    Add sign-up and error messages

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -35,7 +35,7 @@ class UserTest < ActiveSupport::TestCase
   end
   
   test "email validation should reject invalid addresses" do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses = %w[user@foo..com user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?
@@ -49,8 +49,16 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
   
+   test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPLe.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+  
   test "password should have minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
 end
