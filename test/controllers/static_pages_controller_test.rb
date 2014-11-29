f71acddda9108deff9b52df91bef1e37b782222c
thebleakness <janik.knittle@gmail.com>
2014-11-29 17:42:39 +0000

    Update to ruby 2.1.5 and other gems

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = "Kombustion : It's gonna explode"
   end
   
   test "should get home" do
